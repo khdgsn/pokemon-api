5abd400c9ed2fe71a7d2d1871464059923e0b05a
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-01-31 18:43:45 +0000

    update code/ typing errors

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const pokedex = document.getElementById("pokedex");
 
-const promises = [];
-for(let i = 1; i <- 150; i++){
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then(res => res.json()));
+const fetchPokemon = () => {
+    const promises = [];
+    for(let i = 1; i <= 150; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then(res => res.json()));
+    }
+    
+    Promise.all(promises).then(results => {
+        const pokemon = results.map(data => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites["front_default"],
+            type: data.types.map(type => type.type.name).join(", "),
+        }));
+        displayPokemon(pokemon);
+    });
 }
-Promise.all(promises).then(results => {
-    const pokemon = results.map(data => ({
-        name: data.name,
-        id: data.id,
-        image: data.sprites["front_default"],
-        type: data.types.map(type => type.type.name).join(", "),
-    }));
-    displayPokemon(pokemon);
-});
-
 
 
 const displayPokemon = pokemon => {
@@ -32,3 +34,5 @@ const displayPokemon = pokemon => {
         .join("");
     pokedex.innerHTML = pokemonHTMLString;
 };
+
+fetchPokemon()
